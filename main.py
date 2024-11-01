01689db2f58c51944a79d4776527ac50f105d674
Maciek Wełpa <101259499+welmac0@users.noreply.github.com>
2024-11-01 17:00:51 +0100

    Add main and gitignore

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, abort, render_template, redirect, url_for, flash, send_file
+from flask_bootstrap import Bootstrap
+import json
+import os
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    with open("static/files/source.json") as content:
+        content = json.loads(content.read())
+    return render_template("index.html", content=content)
+
+
+@app.route('/iwantresume')
+def give_resume():
+    file = f'static/files/resume.pdf'
+    return send_file(file, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+
+
+
